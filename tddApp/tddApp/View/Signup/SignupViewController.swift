64dbf68f406bf1bcc131663fb31a39fe482408b5
ELO :) <eliasjan@ciandt.com>
2022-06-22 03:22:48 -0300

    add init files

---
+++ tddApp/tddApp/View/Signup/SignupViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignupViewController.swift
+//  tddApp
+//
+//  Created by cit on 22/06/22.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+
+    }
+}
---
+++ tddApp/tddAppTests/SignupFormModelValidatorTests.swift
@@ -0,0 +1,19 @@
+//
+//  SignupFormModelValidatorTests.swift
+//  tddAppTests
+//
+//  Created by cit on 22/06/22.
+//
+
+import XCTest
+@testable import tddApp
+
+class SignupFormModelValidatorTests: XCTestCase {
+
+    override func setUpWithError() throws {}
+    override func tearDownWithError() throws {}
+
+    func testExample() throws {
+
+    }
+}
